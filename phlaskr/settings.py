7e0dcc9380f11affd7155f84dbff416de2b419d0
kyle roux <kyle@level2designs.com>
2015-07-03 00:12:31 -0700

    fix

--- phlaskr/settings.py
+++ phlaskr/settings.py
@@ -3,14 +3,15 @@ from local_settings import LocalConfig
 
 class BaseConfig(LocalConfig):
 
-    DATABASE_URI = 'sqlite:///test3.db'
+    DATABASE_URI = os.environ.get('DATABASE_URL')
     SECRET_KEY = 'testing'
     COOKIE_TTL = 100060
     DEBUG = True
 
 
 class ProductionConfig(BaseConfig):
-    DATABASE_URI = 'postgres://boayqxuajzoyaz:7dI-lm3A028GIqWwgdY1LsHAl6@ec2-54-227-247-161.compute-1.amazonaws.com:5432/d19d1cqd47ej3e'#'mysql+pymysql://fab:fab@localhost:3306/fabtest'#os.environ.get('CLEARDB_DATABASE_URL').replace('mysql://','mysql+pymysql://')#'mysql+pymysql://test:test@localhost:3306/test'
+    DATABASE_URI = os.environ.get('DATABASE_URL')
+
     DEBUG = False
 
 class TestConfig(BaseConfig):
